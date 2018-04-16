cffa295ae24c654fb110f56742183bc8f0309818
占益民 <allen@peytondeimac.local>
2018-04-16 13:13:21 +0800

    Initial Commit

---
+++ CutImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CutImage
+//
+//  Created by Peyton on 2018/4/16.
+//  Copyright © 2018年 Peyton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CutImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CutImage
+//
+//  Created by Peyton on 2018/4/16.
+//  Copyright © 2018年 Peyton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
